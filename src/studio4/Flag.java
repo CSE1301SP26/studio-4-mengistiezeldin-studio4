8d492af8eeb0b479242d5a6690bf8d7a8fe78f93
elroemengistie <e.mengistie@wustl.edu>
2026-02-18 15:35:19 -0600

    Finished

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,10 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(6, 11, 156);
+		StdDraw.filledRectangle(.165, .5, .165, .5);
+		StdDraw.setPenColor(184, 7, 7);
+		StdDraw.filledRectangle(.83, .5, .165, .5);
+	
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,26 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape = in.next();
+		int int1 = in.nextInt();
+		int int2 = in.nextInt();
+		int int3 = in.nextInt();
+		boolean spaceHolder = in.nextBoolean();
+		double double1 = in.nextDouble();
+		double double2 = in.nextDouble();
+		double double3 = in.nextDouble();
+		double double4 = in.nextDouble();
+		if (shape.equals("rectangle")) {
+			StdDraw.rectangle(double1, double2, double3, double4);
+		} else if (shape.equals("triangle")) {
+			double double5 = in.nextDouble();
+			double double6 = in.nextDouble();
+			double[] x = {double1, double3, double5};
+			double[] y = {double2, double4, double6};
+			StdDraw.setPenColor(int1, int2, int3);
+			StdDraw.filledPolygon(x, y);
+		} else {
+			StdDraw.ellipse(double1, double2, double3, double4);
+		}
 	}
 }
